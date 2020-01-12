9837c9876f288c0c83ef349601c5e5ba96c736d5
Kelley van Evert <hello@kelleyvanevert.nl>
2020-01-12 11:51:55 +0100

    markers for @monaco-editor/react ?

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useRef } from "react";
 import stripIndent from "common-tags/lib/stripIndent";
 
-import gen from "./lib/gen";
+import { ControlledEditor } from "@monaco-editor/react";
+
+// import gen from "./lib/gen";
 
 import "./App.scss";
 
@@ -41,36 +43,122 @@ const CODE_EXAMPLES = [
   `
 ];
 
+const EDITOR_OPTIONS = {
+  acceptSuggestionOnCommitCharacter: true,
+  acceptSuggestionOnEnter: "on",
+  accessibilitySupport: "auto",
+  autoIndent: false,
+  automaticLayout: true,
+  codeLens: true,
+  colorDecorators: true,
+  contextmenu: false,
+  cursorBlinking: "blink",
+  cursorSmoothCaretAnimation: false,
+  cursorStyle: "line",
+  disableLayerHinting: false,
+  disableMonospaceOptimizations: false,
+  dragAndDrop: false,
+  fixedOverflowWidgets: false,
+  folding: false,
+  foldingStrategy: "auto",
+  fontLigatures: false,
+  formatOnPaste: false,
+  formatOnType: false,
+  hideCursorInOverviewRuler: false,
+  highlightActiveIndentGuide: true,
+  links: true,
+  mouseWheelZoom: false,
+  multiCursorMergeOverlapping: true,
+  multiCursorModifier: "alt",
+  overviewRulerBorder: true,
+  overviewRulerLanes: 2,
+  quickSuggestions: true,
+  quickSuggestionsDelay: 100,
+  readOnly: false,
+  renderControlCharacters: false,
+  renderFinalNewline: true,
+  renderIndentGuides: true,
+  renderLineHighlight: "all",
+  renderWhitespace: "none",
+  revealHorizontalRightPadding: 30,
+  roundedSelection: true,
+  rulers: [],
+  scrollBeyondLastColumn: 5,
+  scrollBeyondLastLine: true,
+  selectOnLineNumbers: true,
+  selectionClipboard: true,
+  selectionHighlight: true,
+  showFoldingControls: "mouseover",
+  smoothScrolling: false,
+  suggestOnTriggerCharacters: true,
+  wordBasedSuggestions: true,
+  wordSeparators: "~!@#$%^&*()-=+[{]}|;:'\",.<>/?",
+  wordWrap: "off",
+  wordWrapBreakAfterCharacters: "\t})]?|&,;",
+  wordWrapBreakBeforeCharacters: "{([+",
+  wordWrapBreakObtrusiveCharacters: ".",
+  wordWrapColumn: 80,
+  wordWrapMinified: true,
+  wrappingIndent: "none"
+};
+
 export default function App() {
   const [code, set_code] = useState(CODE_EXAMPLES[0]);
+  const editorRef = useRef();
 
-  const generated_code = useMemo(() => gen(code), [code]);
+  // const generated_code = useMemo(() => gen(code), [code]);
 
-  const student_app = useMemo(() => {
-    if (!generated_code) return;
+  // const student_app = useMemo(() => {
+  //   if (!generated_code) return;
 
-    try {
-      window.app = eval(generated_code);
-      return <p>successfully compiled app</p>;
-    } catch (e) {
-      console.error("COULD NOT COMPILE:", e, generated_code);
-      return (
-        <p>
-          <strong>COULD NOT COMPILE</strong>
-        </p>
-      );
-    }
-  }, [generated_code]);
+  //   try {
+  //     window.app = eval(generated_code);
+  //     return <p>successfully compiled app</p>;
+  //   } catch (e) {
+  //     console.error("COULD NOT COMPILE:", e, generated_code);
+  //     return (
+  //       <p>
+  //         <strong>COULD NOT COMPILE</strong>
+  //       </p>
+  //     );
+  //   }
+  // }, [generated_code]);
 
   return (
     <div>
-      <textarea
+      <ControlledEditor
+        editorDidMount={(e, editor) => {
+          editorRef.current = editor;
+
+          const model = (window.model = editor.getModel());
+          console.log(editor, model);
+
+          // editor.setModelMarkers(editor.getModel(), null, [
+          //   {
+          //     startLineNumber: 2,
+          //     startColumn: 1,
+          //     endLineNumber: 1,
+          //     endColumn: 8,
+          //     message: "message",
+          //     severity: 4
+          //   }
+          // ]);
+        }}
+        height={400}
+        width={600}
+        value={code}
+        onChange={(e, code) => set_code(code)}
+        language="javascript"
+        theme="tomorrow-night" // todo
+        options={EDITOR_OPTIONS}
+      />
+      {/* <textarea
         className="student_code"
         value={code}
         onChange={e => set_code(e.target.value)}
       />
       <pre className="generated_code">{generated_code}</pre>
-      {<div className="student_app">{student_app}</div>}
+      {<div className="student_app">{student_app}</div>} */}
     </div>
   );
 }
